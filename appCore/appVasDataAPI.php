baa7b73f97fa80c23f1fcdfdb1ced18f524429ec
Mauricio Luiz Schiavon Marini <marini.mauricio@gmail.com>
2021-09-27 21:43:01 -0300

    WIP - API + APP

--- appCore/appVasDataAPI.php
+++ appCore/appVasDataAPI.php
@@ -9,22 +9,30 @@ use app\System\API\appDataAPI;
 $v_dataSec = !empty($_REQUEST['dataSec']) ? $_REQUEST['dataSec'] : NULL;
 
 if($v_dataSec == "customerTokenData") {
-    $v_appData = !empty($_REQUEST) ? $_REQUEST : NULL;
     $v_appRequest = !empty($_REQUEST) ? $_REQUEST : NULL;
     $v_appData = new appDataAPI();
     $v_apiData = $v_appData->appCustomerTokenData($v_appRequest);
 
-    print_r($v_apiData);
-    die();
+    if($v_apiData['customer_status'] == 1){
+        $v_apiData['customer_status'] = true;
+    } else {
+        $v_apiData['customer_status'] = false;
+    }
+    $v_apiData['lastUpdate'] = date('d/m/Y H:i:s');
 
     header('Content-Type: application/json; charset=utf-8');
-    echo json_encode($v_returnData);
+    echo json_encode($v_apiData);
 }
 elseif ($v_dataSec == "checkAppConnection") {
     $v_appRequest = !empty($_REQUEST) ? $_REQUEST : NULL;
     $v_appData = new appDataAPI();
     $v_apiData = $v_appData->appCustomerLicenceData($v_appRequest);
-    $v_returnData['customerLicence'] = $v_apiData['rsData'][0]['customer_status'] == 1 ? true : false;
+
+    if($v_apiData['customer_status'] == 1) {
+        $v_returnData['customerLicence'] = true;
+    }else{
+        $v_returnData['customerLicence'] = false;
+    }
     $v_returnData['ping'] = true;
     $v_returnData['lastUpdate'] = date('d/m/Y H:i:s');
     header('Content-Type: application/json; charset=utf-8');
